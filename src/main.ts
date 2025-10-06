54328b334ee70e32d63776d38c853d2529e86d16
cleitonfonseca <cleitonfonseca@boltcard.com.br>
2025-10-06 19:56:31 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:8081', 'http://localhost:19006'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   console.log('http://localhost/' + (process.env.PORT ?? 3000));
   await app.listen(process.env.PORT ?? 3000);
 }
